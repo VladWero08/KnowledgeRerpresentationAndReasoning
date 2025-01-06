cce4293c5e02278248fa9ddef9064758bc6766ed
VladWero08 <vlad_wero@yahoo.com>
2025-01-06 13:25:35 +0200

    updated the goal choice method in the backward chaining sld resolution

--- sld-resolution-and-vagueness/sld-backward.pl
+++ sld-resolution-and-vagueness/sld-backward.pl
@@ -2,20 +2,16 @@
 
 make_new_goals(Clause, Goals, Atom, GoalsNew) :-
     eliminate(Atom, Clause, ClauseUpdated),
-    neg(Atom, NAtom),
-    eliminate(NAtom, Goals, GoalsUpdated),
-    merge(ClauseUpdated, GoalsUpdated, GoalsNew).
+    merge(ClauseUpdated, Goals, GoalsNew).
     
-is_clause_resolving(Clause, Goals, GoalsNew) :-
+is_clause_resolving(Clause, [Goal|Goals], GoalsNew) :-
     member(CAtom, Clause),
-    member(GAtom, Goals),
-    is_opposite(CAtom, GAtom),
+    is_opposite(CAtom, Goal),
     make_new_goals(Clause, Goals, CAtom, GoalsNew), !.
 
-resolution_backward(_, []) :- write("YES").
+resolution_backward(_, []).
 resolution_backward(KB, Goals) :- 
-    write(Goals),
     member(Clause, KB),
     is_clause_resolving(Clause, Goals, GoalsNew),
-    resolution_backward(KB, GoalsNew), !.
-resolution_backward(_, _) :- write("NO"), false.
+    resolution_backward(KB, GoalsNew).
+resolution_backward(_, _) :- false.
