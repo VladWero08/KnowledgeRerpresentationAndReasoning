dec3a8f9ca247966b2646c43b51642815f4585f5
VladWero08 <vlad_wero@yahoo.com>
2025-01-09 22:06:47 +0200

    finished the degree curves predicates and price computing

--- sld-resolution-and-vagueness/degree-curves.pl
+++ sld-resolution-and-vagueness/degree-curves.pl
@@ -1,10 +1,4 @@
-min(A, B, A) :- A < B, !.
-min(_, B, B).
-
-max(A, B, A) :- A > B, !.
-max(_, B, B).
-
-
+:- ["./utils/utils.pl"].
 
 new_aged(X, Degree) :- X >= 0, X =< 2, Degree is 1.
 new_aged(X, Degree) :- X > 2, X =< 5, Degree is (1 - (X - 2) / 3).
@@ -19,7 +13,6 @@ middle_aged(X, Degree) :- X >= 10, X =< 15, Degree is 0.
 old_aged(X, Degree) :- X >= 0, X =< 7, Degree is 0.
 old_aged(X, Degree) :- X > 7, X < 10, Degree is (X / 3 - 7 / 3).
 old_aged(X, Degree) :- X >= 10, X =< 15, Degree is 1.
-
 
 
 low_consum(X, Degree) :- X >= 0, X =< 1, Degree is 1.
@@ -37,7 +30,6 @@ high_consum(X, Degree) :- X > 7, X < 12, Degree is (1 / 5) * (X - 7).
 high_consum(X, Degree) :- X >= 12, X =< 15, Degree is 1.
 
 
-
 low_mileage(X, Degree) :- X >= 0, X =< 50000, Degree is 1.
 low_mileage(X, Degree) :- X > 50000, X =< 100000, Degree is 1 - (X - 50000) / 50000.
 low_mileage(X, Degree) :- X > 100000, X =< 200000, Degree is 0.
@@ -51,7 +43,6 @@ medium_mileage(X, Degree) :- X >= 170000, X =< 200000, Degree is 0.
 high_mileage(X, Degree) :- X >= 0, X =< 150000, Degree is 0.
 high_mileage(X, Degree) :- X > 150000, X < 190000, Degree is (X - 150000) / 40000.
 high_mileage(X, Degree) :- X >= 190000, X =< 200000, Degree is 1.
-
 
 
 low_price(X, Degree) :- X >= 0, X =< 2, Degree is 1.
@@ -68,8 +59,56 @@ high_price(X, Degree) :- X >= 0, X =< 8, Degree is 0.
 high_price(X, Degree) :- X > 8, X < 13, Degree is (X - 8) / 5.
 high_price(X, Degree) :- X >= 13, X =< 15, Degree is 1.
 
+
 apply_predicates([], _, []).
 apply_predicates([Attribute/Predicate | Predicates], AttributeValues, [Degree | Degrees]) :-
     member(Attribute/Value, AttributeValues),
     call(Predicate, Value, Degree),
     apply_predicates(Predicates, AttributeValues, Degrees).
+
+apply_conjuction([], 1).
+apply_conjuction([Probability | Probabilities], R) :- 
+    apply_conjuction(Probabilities, RRecursive),
+    min(Probability, RRecursive, R).
+
+apply_disjunction([], 0).
+apply_disjunction([Probability | Probabilities], R) :-
+    apply_disjunction(Probabilities, RRecursive),
+    max(Probability, RRecursive, R).
+
+apply_premises(Operator, Premises, Inputs, Result) :-
+    Operator = and,
+    apply_predicates(Premises, Inputs, Degrees),
+    apply_conjuction(Degrees, Result).
+apply_premises(Operator, Premises, Inputs, Result) :-
+    Operator = or,
+    apply_predicates(Premises, Inputs, Degrees),
+    apply_disjunction(Degrees, Result).
+
+apply_min_premises([], _, []).
+apply_min_premises([Premise|Premises], Consequence, [Min|Mins]) :-
+    min(Premise, Consequence, Min),
+    apply_min_premises(Premises, Consequence, Mins).
+
+apply_rules([], _, []).
+apply_rules([[Operator, Premises, [_/Consequence]]|KB], Inputs, Result) :-
+    apply_premises(Operator, Premises, Inputs, ResultPremises),
+    findall(
+        ResultConsequence,
+        (between(0, 15, X), call(Consequence, X, ResultConsequence)), 
+        ResultsConsequence
+    ),
+    apply_min_premises(ResultsConsequence, ResultPremises, ResultCurr),
+    apply_rules(KB, Inputs, ResultRecurr),
+	max_between_lists(ResultCurr, ResultRecurr, Result).
+
+get_centroid_price(Ys, Price) :-
+	interval(0, 15, Xs),
+    sum(Ys, YSum),
+    sum_between_lists_product(Xs, Ys, XYSum),
+    Price is XYSum / YSum.
+
+get_price(KB, Inputs, Price) :-
+	apply_rules(KB, Inputs, Ys),
+    get_centroid_price(Ys, Price).
+       
--- sld-resolution-and-vagueness/utils/utils.pl
+++ sld-resolution-and-vagueness/utils/utils.pl
@@ -11,3 +11,28 @@ eliminate(Target, [H|T], [H|Result]) :- eliminate(Target, T, Result).
 merge([], L, L).
 merge([H|T], L, Result) :- member(H, L), !, merge(T, L, Result).
 merge([H|T], L, [H|Result]) :- merge(T, L, Result).
+
+interval(Max, Max, [Max]).
+interval(Min, Max, [Min|Rest]) :- 
+    Min < Max, 
+    NewMin is Min + 1,
+    interval(NewMin, Max, Rest).
+
+min(A, B, A) :- A < B, !.
+min(_, B, B).
+
+max(A, B, A) :- A > B, !.
+max(_, B, B).
+
+max_between_lists([], [], []).
+max_between_lists([A|As], [], [A|Rs]) :- max_between_lists(As, [], Rs).
+max_between_lists([], [B|Bs], [B|Rs]) :- max_between_lists(Bs, [], Rs).
+max_between_lists([A|As], [B|Bs], [R|Rs]) :- max(A, B, R), max_between_lists(As, Bs, Rs).
+
+sum([], 0).
+sum([H|T], Sum) :- sum(T, SumRec), Sum is H + SumRec.
+
+sum_between_lists_product([], [], 0).
+sum_between_lists_product([A|As], [B|Bs], Sum) :- 
+    sum_between_lists_product(As, Bs, SumRec),
+    Sum is SumRec + A * B.
