5fdea30e299eb92dd62251f1d99c60d820060201
VladWero08 <vlad_wero@yahoo.com>
2025-01-24 22:16:26 +0200

    updated the sld backward algorithm

--- sld-resolution/sld-backward.pl
+++ sld-resolution/sld-backward.pl
@@ -1,21 +1,25 @@
 :- ["./utils/utils.pl"].
 
-make_new_goals(Clause, Goals, Atom, GoalsNew) :-
-    eliminate(Atom, Clause, ClauseUpdated),
-    merge(ClauseUpdated, Goals, GoalsNew).
-    
-is_clause_resolving(Clause, [Goal|Goals], GoalsNew) :-
-    member(CAtom, Clause),
-    is_opposite(CAtom, Goal),
-    make_new_goals(Clause, Goals, CAtom, GoalsNew).
+get_positive_atom(Clause, Atom) :- member(Atom, Clause), is_pos(Atom), !.
 
-resolution_backward_helper(_, []).
-resolution_backward_helper(KB, Goals) :- 
+make_new_goals(Clause, Goals, PAtom, NAtom, GoalsNew) :-
+    eliminate(PAtom, Clause, ClauseUpdated),
+    eliminate(NAtom, Goals, GoalsUpdated),
+    merge(ClauseUpdated, GoalsUpdated, GoalsNew).
+    
+is_clause_resolving(Clause, Goals, GoalsNew) :-
+    get_positive_atom(Clause, PAtom),
+    member(Goal, Goals),
+    is_opposite(PAtom, Goal),
+    make_new_goals(Clause, Goals, PAtom, Goal, GoalsNew).
+
+resolution_backward_helper(_, [], "YES") :- !.
+resolution_backward_helper(KB, Goals, Result) :-
     member(Clause, KB),
     is_clause_resolving(Clause, Goals, GoalsNew),
-    resolution_backward_helper(KB, GoalsNew).
-resolution_backward_helper(_, _) :- false.
+    resolution_backward_helper(KB, GoalsNew, Result).
+resolution_backward_helper(_, _, "NO").
 
-resolution_backward(KB, Goals) :-
+resolution_backward(KB, Goals, Result) :-
     neg_list(Goals, GoalsNeg),
-    resolution_backward(KB, GoalsNeg).
+    resolution_backward_helper(KB, GoalsNeg, Result).
